b8b7a20cafe9c3704ebedddefc667cd39bb07265
James Welbes <james.welbes@gmail.com>
2021-06-10 18:43:47 -0500

    finish columns hero

--- wp-content/themes/apex-branding-design/page-templates/home.php
+++ wp-content/themes/apex-branding-design/page-templates/home.php
@@ -9,14 +9,14 @@
                     <h1 class="hero-header">We help businesses elevate their brand through the power of strategy, design and creativity.</h1>
 					 <div class="hero-tagline">
                         <p>Has your product or service outgrown your brand? We can help.</p>
-                        <p>Check out Angie's rebrand story <i class="fas fa-arrow-right shake-horizontal ms-3"></i></p>
+                        <p>Check out Angie's rebrand story <i class="fas fa-arrow-right shake-horizontal ms-3"></i> <i class="fas fa-arrow-down shake-vertical ms-3"></i></p>
                      </div>
 				
 					<!-- <a href="/contact"><button class="btn btn-primary hero-btn">Get Started</button></a> -->
                     <!-- <button class="btn btn-secondary hero-btn" id="show-video"><i class="far fa-play-circle"></i> Watch Video</button> -->
                
                 </div>
-                <div class="col-md-6 col-sm-12" id="home-case-stud"><iframe id="angie-video" type="text/html" width="640" height="360" src="https://www.youtube-nocookie.com/embed/H5-HaRzq4Wc?modestbranding=1" frameborder="0"></iframe></div>
+                <div class="col-md-6 col-sm-12 flex-column" id="home-case-stud"><iframe id="angie-video" type="text/html" width="640" height="360" src="https://www.youtube-nocookie.com/embed/H5-HaRzq4Wc?modestbranding=1" frameborder="0"></iframe></div>
             </div>
         </div>
     </div>
