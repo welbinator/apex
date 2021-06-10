10ec69fe949570500d226a2660d9d991d7db5bad
James Welbes <james.welbes@gmail.com>
2021-06-09 23:29:42 -0500

    updated gitignore

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1623797223;s:7:"authKey";s:64:"4WJ2asZ/#~ Hh*v7#guT:HxI!{/vl37/yI7+}Lp>v~`2+3h0~cl|Ji+9EX3u>:ES";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3540;}
