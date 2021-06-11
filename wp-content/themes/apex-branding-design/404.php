b0c13deaaa4b5f025deba57f811f5f883f86c087
James Welbes <james.welbes@gmail.com>
2021-06-10 19:01:20 -0500

    style 404

--- wp-content/themes/apex-branding-design/404.php
+++ wp-content/themes/apex-branding-design/404.php
@@ -7,7 +7,7 @@
  * @package Apex_Branding_&_Design
  */
 
-get_header();
+get_header('apex');
 ?>
 
 	<main id="primary" class="site-main">
@@ -20,36 +20,9 @@ get_header();
 			<div class="page-content">
 				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'apex-branding-design' ); ?></p>
 
-					<?php
-					get_search_form();
+					
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'apex-branding-design' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$apex_branding_design_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'apex-branding-design' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$apex_branding_design_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
@@ -57,4 +30,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_footer();
+get_footer('apex');
